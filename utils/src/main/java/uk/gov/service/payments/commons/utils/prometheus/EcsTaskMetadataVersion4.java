18fc54e9d8ccb81a79c4e644d666a3f2423c3f9b
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2023-07-19 12:56:39 +0100

    get the Container correspoding to the application

--- utils/src/main/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4.java
+++ utils/src/main/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4.java
@@ -1,10 +1,14 @@
 package uk.gov.service.payments.commons.utils.prometheus;
 
+import org.eclipse.jetty.util.ajax.JSON;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Extracts relevant information from the <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html">AWS ECS task metadata endpoint version 4</a>.
@@ -23,21 +27,19 @@ public class EcsTaskMetadataVersion4 {
      * Will throw a runtime exception if JSON is not valid.
      * @param ecsTaskMetadataJson A valid <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html#task-metadata-endpoint-v4-response">Task metadata JSON</a>
      */
-    public EcsTaskMetadataVersion4(String ecsTaskMetadataJson) {
+    public EcsTaskMetadataVersion4(String ecsTaskMetadataJson, String application) {
         this.ecsTaskMetadataJson = new JSONObject(ecsTaskMetadataJson);
         
         String cluster = this.ecsTaskMetadataJson.getString("Cluster");
         this.clusterName = cluster.substring(cluster.lastIndexOf("/") + 1);
-
         this.accountName = clusterName.substring(0, clusterName.indexOf("-"));
         
         String taskARN = this.ecsTaskMetadataJson.getString("TaskARN");
         this.ecsTaskId = taskARN.substring(taskARN.lastIndexOf("/") + 1);
 
-        JSONObject container = this.ecsTaskMetadataJson.getJSONArray("Containers").getJSONObject(0);
+        JSONObject container = getApplicationContainer(application);
         String image = container.getString("Image");
         this.containerImageTag = image.substring(image.lastIndexOf(":") + 1);
-        
         this.serviceName = container.getString("Name");
 
         String instanceLabel;
@@ -49,6 +51,18 @@ public class EcsTaskMetadataVersion4 {
         }
         this.instanceLabel = instanceLabel;
             
+    }
+
+    private JSONObject getApplicationContainer(String application) {
+        JSONArray containers = this.ecsTaskMetadataJson.getJSONArray("Containers");
+        List applicationContainer = containers.toList().stream().filter(
+                c -> new JSONObject((Map) c).getString("Image").contains("govukpay/" + application.strip())
+        ).collect(Collectors.toList());
+        if (applicationContainer.isEmpty()) {
+            throw new JSONException("The ecs task metadata json does not have the key 'Containers'.");
+        }
+        JSONObject container = new JSONObject((Map) applicationContainer.get(0));
+        return container;
     }
 
     public String getContainerImageTag() {
