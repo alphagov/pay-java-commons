b6ef80f078ed97ac54b7f5f3d406a4a4f2638018
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2023-07-19 10:17:56 +0100

    [to be squashed]: constructor must take in a valid task metadata json

--- utils/src/main/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4.java
+++ utils/src/main/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4.java
@@ -1,10 +1,6 @@
 package uk.gov.service.payments.commons.utils.prometheus;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.Optional;
 
 /**
  * Extracts relevant information from the <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html">AWS ECS task metadata endpoint version 4</a>.
@@ -12,56 +8,51 @@ import java.util.Optional;
 public class EcsTaskMetadataVersion4 {
     
     private final JSONObject ecsTaskMetadataJson;
+    private final String clusterName;
+    private final String accountName;
+    private final String ecsTaskId;
+    private final String containerImageTag;
+    private final String serviceName;
+
+    /**
+     * Will throw a runtime exception if JSON is not valid.
+     * @param ecsTaskMetadataJson A valid <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html#task-metadata-endpoint-v4-response">Task metadata JSON</a>
+     */
     public EcsTaskMetadataVersion4(String ecsTaskMetadataJson) {
         this.ecsTaskMetadataJson = new JSONObject(ecsTaskMetadataJson);
+        
+        String cluster = this.ecsTaskMetadataJson.getString("Cluster");
+        this.clusterName = cluster.substring(cluster.lastIndexOf("/") + 1);
+
+        this.accountName = clusterName.substring(0, clusterName.indexOf("-"));
+        
+        String taskARN = this.ecsTaskMetadataJson.getString("TaskARN");
+        this.ecsTaskId = taskARN.substring(taskARN.lastIndexOf("/") + 1);
+
+        JSONObject container = this.ecsTaskMetadataJson.getJSONArray("Containers").getJSONObject(0);
+        String image = container.getString("Image");
+        this.containerImageTag = image.substring(image.lastIndexOf(":") + 1);
+        
+        this.serviceName = container.getString("Name");
     }
 
-    public Optional<String> getContainerImageTag() {
-        return getJSONArray(ecsTaskMetadataJson, "Containers").map(array -> {
-            if (array.isEmpty()) {
-                return null;
-            }
-            String image = array.getJSONObject(0).getString("Image");
-            return image.substring(image.lastIndexOf(":") + 1);
-        });
+    public String getContainerImageTag() {
+        return containerImageTag;
     }
 
-    public Optional<String> getClusterName() {
-        return getString(ecsTaskMetadataJson, "Cluster").map(cluster ->
-            cluster.substring(cluster.lastIndexOf("/") + 1));
-    }
-    
-    private static Optional<String> getString(JSONObject jsonObject, String key) {
-        try {
-            return Optional.of(jsonObject.getString(key));
-        } catch (JSONException e) {
-            return Optional.empty();
-        }
-    }
-    
-    private static Optional<JSONArray> getJSONArray(JSONObject jsonObject, String key) {
-        try {
-            return Optional.of(jsonObject.getJSONArray(key));
-        } catch (JSONException e) {
-            return Optional.empty();
-        }
+    public String getClusterName() {
+        return clusterName;
     }
 
-    public Optional<String> getServiceName() {
-        return getJSONArray(ecsTaskMetadataJson, "Containers").map(array -> {
-            if (array.isEmpty()) {
-                return null;
-            }
-            return array.getJSONObject(0).getString("Name");
-        });
+    public String getServiceName() {
+        return serviceName;
     }
 
-    public Optional<String> getEcsTaskId() {
-        return getString(ecsTaskMetadataJson, "TaskARN").map(cluster ->
-                cluster.substring(cluster.lastIndexOf("/") + 1));
+    public String getEcsTaskId() {
+        return ecsTaskId;
     }
 
-    public Optional<String> getAccountName() {
-        return getClusterName().map(clusterName -> clusterName.substring(0, clusterName.indexOf("-")));
+    public String getAccountName() {
+        return accountName;
     }
 }
--- utils/src/test/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4Test.java
+++ utils/src/test/java/uk/gov/service/payments/commons/utils/prometheus/EcsTaskMetadataVersion4Test.java
@@ -1,5 +1,6 @@
 package uk.gov.service.payments.commons.utils.prometheus;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -7,77 +8,44 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EcsTaskMetadataVersion4Test {
     
     private static EcsTaskMetadataVersion4 ecsTaskMetadata;
-    private static EcsTaskMetadataVersion4 ecsTaskMetadataWithoutContainers;
-    
-    private static EcsTaskMetadataVersion4 emptyEcsTaskMetadata;
     
     @BeforeAll
     static void setup() throws IOException {
         ecsTaskMetadata = new EcsTaskMetadataVersion4(new String(EcsTaskMetadataVersion4Test.class.getClassLoader().getResourceAsStream("ecs-container-task-metadata.json").readAllBytes()));
-        ecsTaskMetadataWithoutContainers = new EcsTaskMetadataVersion4(new String(EcsTaskMetadataVersion4Test.class.getClassLoader().getResourceAsStream("ecs-metadata-no-containers.json").readAllBytes()));
-        emptyEcsTaskMetadata = new EcsTaskMetadataVersion4("{}");
     }
     
     @Test
-    void extractContainerImageTag() throws Exception {
-        assertEquals("PP-11223-4", ecsTaskMetadata.getContainerImageTag().get());
-    }
-    
-    @Test
-    void shouldReturnEmptyContainerImageTag() {
-        assertTrue(ecsTaskMetadataWithoutContainers.getContainerImageTag().isEmpty());
-        assertTrue(emptyEcsTaskMetadata.getContainerImageTag().isEmpty());
+    void extractContainerImageTag() {
+        assertEquals("PP-11223-4", ecsTaskMetadata.getContainerImageTag());
     }
     
     @Test
     void shouldThrowExceptionIfInvalidJsonProvided() {
-        assertThrows(RuntimeException.class, () -> new EcsTaskMetadataVersion4("{\"Cluster\":\"arn\""));
+        assertThrows(JSONException.class, () -> new EcsTaskMetadataVersion4("{\"Cluster\":\"arn\""));
+        assertThrows(JSONException.class, () -> new EcsTaskMetadataVersion4(new String(EcsTaskMetadataVersion4Test.class.getClassLoader().getResourceAsStream("ecs-metadata-no-containers.json").readAllBytes())));
     }
     
     @Test
     void extractEcsClusterName() {
-        assertEquals("test-12-fargate", ecsTaskMetadata.getClusterName().get());
-    }
-    
-    @Test
-    void shouldReturnEmptyClusterName() {
-        assertTrue(emptyEcsTaskMetadata.getClusterName().isEmpty());
+        assertEquals("test-12-fargate", ecsTaskMetadata.getClusterName());
     }
     
     @Test
     void extractEcsServiceName() {
-        assertEquals("connector", ecsTaskMetadata.getServiceName().get());
-    }
-
-    @Test
-    void shouldReturnEmptyEcsServiceName() {
-        assertTrue(ecsTaskMetadataWithoutContainers.getServiceName().isEmpty());
-        assertTrue(emptyEcsTaskMetadata.getServiceName().isEmpty());
+        assertEquals("connector", ecsTaskMetadata.getServiceName());
     }
 
     @Test
     void extractEcsTaskId() {
-        assertEquals("b322172c022b4a8c9411f923350cb623", ecsTaskMetadata.getEcsTaskId().get()); // pragma: allowlist secret
+        assertEquals("b322172c022b4a8c9411f923350cb623", ecsTaskMetadata.getEcsTaskId()); // pragma: allowlist secret
     }
-
-    @Test
-    void shouldReturnEmptyEcsTaskId() {
-        var ecsTaskMetadata = new EcsTaskMetadataVersion4("{}");
-        assertTrue(ecsTaskMetadata.getEcsTaskId().isEmpty());
-    }
-
+    
     @Test
     void extractAccountName() {
-        assertEquals("test", ecsTaskMetadata.getAccountName().get());
-    }
-
-    @Test
-    void shouldReturnEmptyAccountName() {
-        assertTrue(emptyEcsTaskMetadata.getAccountName().isEmpty());
+        assertEquals("test", ecsTaskMetadata.getAccountName());
     }
 }
