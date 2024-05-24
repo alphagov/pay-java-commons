15ff1083d404fcbb8117d493b69585c6654f52ff
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2024-05-24 14:03:08 +0100

    PP-12177 impelement despotifydockerification
    Refactor to remove Spotify docker. Replace it with testcontainer.

--- testing/src/main/java/uk/gov/service/payments/commons/testing/db/PostgresContainer.java
+++ testing/src/main/java/uk/gov/service/payments/commons/testing/db/PostgresContainer.java
@@ -1,144 +1,115 @@
 package uk.gov.service.payments.commons.testing.db;
 
-import com.google.common.base.Stopwatch;
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.LogStream;
-import com.spotify.docker.client.exceptions.DockerCertificateException;
-import com.spotify.docker.client.exceptions.DockerException;
-import com.spotify.docker.client.messages.ContainerConfig;
-import com.spotify.docker.client.messages.ContainerInfo;
-import com.spotify.docker.client.messages.HostConfig;
-import com.spotify.docker.client.messages.LogConfig;
-import com.spotify.docker.client.messages.PortBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.List;
+import java.sql.SQLException;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.String.format;
-import static java.util.stream.Collectors.joining;
+import static java.sql.DriverManager.getConnection;
 
 public class PostgresContainer {
 
     private static final Logger logger = LoggerFactory.getLogger(PostgresContainer.class);
 
-    private final String containerId;
-    private final int port;
-    private final DockerClient docker;
-    private volatile boolean stopped = false;
-    private final String dbPassword;
-    private final String dbUsername;
 
-    private static final int DB_TIMEOUT_SEC = 15;
-    private static final String INTERNAL_PORT = "5432";
+    private static PostgreSQLContainer<?> POSTGRES_CONTAINER;
+    private static String DB_NAME = "pay_test";
+    private static String DB_USERNAME = "test";
+    private static String DB_PASSWORD = "test";
+    private static String IMAGE_TAG = "15.2";
 
-    public PostgresContainer(DockerClient docker, String imageName, String dbUsername, String dbPassword) throws InterruptedException, IOException, DockerException {
-        this.docker = docker;
-        this.dbPassword = dbPassword;
-        this.dbUsername = dbUsername;
-
-        failsafeDockerPull(docker, imageName);
-        docker.listImages(DockerClient.ListImagesParam.create("name", imageName));
-
-        final HostConfig hostConfig = HostConfig.builder().logConfig(LogConfig.create("json-file")).publishAllPorts(true).build();
-        ContainerConfig containerConfig = ContainerConfig.builder()
-                .image(imageName)
-                .hostConfig(hostConfig)
-                .env("POSTGRES_USER=" + dbUsername, "POSTGRES_PASSWORD=" + dbPassword)
-                .build();
-        containerId = docker.createContainer(containerConfig).id();
-        docker.startContainer(containerId);
-        port = hostPortNumber(docker.inspectContainer(containerId));
-        registerShutdownHook();
-        waitForPostgresToStart();
+    public PostgresContainer(String imageTag) {
+        new PostgresContainer(DB_USERNAME, DB_PASSWORD, DB_NAME, imageTag);
     }
 
-    public PostgresContainer() throws InterruptedException, IOException, ClassNotFoundException, DockerCertificateException, DockerException {
-        this(DefaultDockerClient.fromEnv().build(), "govukpay/postgres:11.1", "postgres", "mysecretpassword");
+    public PostgresContainer(String dbUsername, String dbPassword, String dbName, String imageTag) {
+        this.DB_PASSWORD = dbPassword;
+        this.DB_USERNAME = dbUsername;
+        this.DB_NAME = dbName;
+        this.IMAGE_TAG = imageTag;
     }
 
-    public PostgresContainer(String imageTag) throws InterruptedException, IOException, DockerCertificateException, DockerException {
-        this(DefaultDockerClient.fromEnv().build(), format("postgres:%s", imageTag), "postgres", "mysecretpassword");
-    }
-
-    public String getUsername() {
-        return dbUsername;
-    }
-
-    public String getPassword() {
-        return dbPassword;
-    }
-
-    public String getConnectionUrl() {
-        return "jdbc:postgresql://" + docker.getHost() + ":" + port + "/";
-    }
-
-    private void failsafeDockerPull(DockerClient docker, String image) {
+    public static void getOrCreate() {
         try {
-            docker.pull(image);
+            if (POSTGRES_CONTAINER == null) {
+                logger.info("Creating Postgres Container");
+
+                POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:" + IMAGE_TAG)
+                        .withUsername(DB_USERNAME)
+                        .withPassword(DB_PASSWORD);
+
+                POSTGRES_CONTAINER.start();
+                createDatabase();
+                registerShutdownHook();
+            }
         } catch (Exception e) {
-            logger.error("Docker image " + image + " could not be pulled from DockerHub", e);
+            throw new RuntimeException(e);
         }
     }
 
-    private static int hostPortNumber(ContainerInfo containerInfo) {
-        List<PortBinding> portBindings = containerInfo.networkSettings().ports().get(INTERNAL_PORT + "/tcp");
-        logger.info("Postgres host port: {}", portBindings.stream().map(PortBinding::hostPort).collect(joining(", ")));
-        return parseInt(portBindings.get(0).hostPort());
+    public static void getOrCreate(String imageTag) {
+        IMAGE_TAG = imageTag;
+        getOrCreate();
     }
 
-    private void registerShutdownHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
+    private static void createDatabase() {
+        try (Connection connection = getConnection(getConnectionUrl(), DB_USERNAME, DB_PASSWORD)) {
+            connection.createStatement().execute("CREATE DATABASE " + DB_NAME + " WITH owner=" + DB_USERNAME + " TEMPLATE postgres");
+            connection.createStatement().execute("GRANT ALL PRIVILEGES ON DATABASE " + DB_NAME + " TO " + DB_USERNAME);
+            connection.createStatement().execute("CREATE EXTENSION IF NOT EXISTS pg_trgm");
+            connection.createStatement().execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void waitForPostgresToStart() throws DockerException, InterruptedException, IOException {
-        Stopwatch timer = Stopwatch.createStarted();
-        boolean succeeded = false;
-        while (!succeeded && timer.elapsed(TimeUnit.SECONDS) < DB_TIMEOUT_SEC) {
-            Thread.sleep(500);
-            succeeded = checkPostgresConnection();
+    public static String getUsername() {
+        return DB_USERNAME;
+    }
+
+    public static String getPassword() {
+        return DB_PASSWORD;
+    }
+
+    public static String getConnectionUrl() {
+        return POSTGRES_CONTAINER.getJdbcUrl();
+    }
+
+    public static PostgreSQLContainer getPostgresContainer() {
+        return POSTGRES_CONTAINER;
+    }
+
+    public static void stop() {
+        if (POSTGRES_CONTAINER == null) {
+            return;
         }
-        if (!succeeded) {
-            throw new RuntimeException("Postgres did not start in " + DB_TIMEOUT_SEC + " seconds.");
+        try {
+            POSTGRES_CONTAINER.stop();
+            POSTGRES_CONTAINER = null;
+        } catch (Exception e) {
+            System.err.println("Could not shutdown Postgres Container");
+            e.printStackTrace();
         }
-        logger.info("Postgres docker container started in {}.", timer.elapsed(TimeUnit.MILLISECONDS));
+    }
+
+    private static void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(PostgresContainer::stop));
     }
 
     private boolean checkPostgresConnection() {
 
         Properties props = new Properties();
-        props.setProperty("user", this.dbUsername);
-        props.setProperty("password", this.dbPassword);
+        props.setProperty("user", this.DB_USERNAME);
+        props.setProperty("password", this.DB_PASSWORD);
 
         try (Connection connection = DriverManager.getConnection(getConnectionUrl(), props)) {
             return true;
         } catch (Exception except) {
             return false;
-        }
-    }
-
-    public void stop() {
-        if (stopped) {
-            return;
-        }
-        try {
-            stopped = true;
-            System.out.println("Killing postgres container with ID: " + containerId);
-            LogStream logs = docker.logs(containerId, DockerClient.LogsParam.stdout(), DockerClient.LogsParam.stderr());
-            System.out.println("Killed container logs:\n");
-            logs.attach(System.out, System.err);
-            docker.stopContainer(containerId, 5);
-            docker.removeContainer(containerId);
-        } catch (DockerException | InterruptedException | IOException e) {
-            System.err.println("Could not shutdown " + containerId);
-            e.printStackTrace();
         }
     }
 }
