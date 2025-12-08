15fdadb3635e70a34cad0e4b3d79a7166529e49a
SandorArpa <sandor.arpa@digital.cabinet-office.gov.uk>
2025-12-08 17:45:17 +0000

    PP-14760 upgrade to Dropwizard v5 iteration 2
    - Remove Dropwizard v4 modules
    - Refactor poms to use the root pom as parent (Dropwizard bom)

--- model-dropwizard-4/src/main/java/uk/gov/service/payments/commons/model/jsonpatch/JsonPatchKeys.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.service.payments.commons.model.jsonpatch;
-
-public interface JsonPatchKeys {
-
-    String FIELD_OPERATION = "op";
-    String FIELD_OPERATION_PATH = "path";
-    String FIELD_VALUE = "value";
-
-}
--- utils-dropwizard-4/src/main/java/uk/gov/service/payments/commons/utils/startup/ApplicationStartupDependentResource.java
+++
@@ -1,5 +0,0 @@
-package uk.gov.service.payments.commons.utils.startup;
-
-interface ApplicationStartupDependentResource {
-    boolean isAvailable();
-}
